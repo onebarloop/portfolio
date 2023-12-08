bfa70ae85deb652dc1dd46e88c389d3d92bf5077
onebarloop <alexanderbayer@posteo.de>
2023-12-08 15:38:08 +0100

    install nuxt/ui

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss', '@nuxtjs/eslint-module'],
+  modules: ['@nuxtjs/eslint-module', '@nuxt/ui'],
   tailwindcss: {
     cssPath: '~/assets/scss/tailwind.scss'
   },
