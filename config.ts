c511d0b164658ef2b93ff0f37d3ef636656aa0a6
onebarloop <alexanderbayer@posteo.de>
2023-12-08 09:55:55 +0100

    add lorem ipsum

--- config.ts
+++ config.ts
@@ -5,14 +5,26 @@ const pages = [
 ]
 
 const vita = [
-  { date: 'since 06/23', text: 'Lorem ipsum...', id: 1 },
-  { date: 'since 07/23', text: 'Lorem ipsum...', id: 2 },
+  {
+    date: 'since 06/23',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    id: 1
+  },
+  {
+    date: 'since 07/23',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    id: 2
+  },
   {
     date: 'since 08/23',
     text: 'Lorem ipsum... ',
     id: 3
   },
-  { date: 'since 09/23', text: 'Lorem ipsum...', id: 4 },
+  {
+    date: 'since 09/23',
+    text: 'Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.',
+    id: 4
+  },
   { date: 'since 10/23', text: 'Lorem ipsum...', id: 5 },
   { date: 'since 11/23', text: 'Lorem ipsum...', id: 6 }
 ]
