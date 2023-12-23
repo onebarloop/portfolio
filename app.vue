326e10ab335b56ba0717bd98c3046e20aa97682d
onebarloop <alexanderbayer@posteo.de>
2023-12-23 15:28:12 +0100

    responsive checks and fixes

--- app.vue
+++ app.vue
@@ -5,7 +5,7 @@ import { pages } from './config'
   <div class="container mx-auto mt-4 px-4 sm:px-12 xl:max-w-[1024px]">
     <header class="sticky top-0 z-20 flex flex-col gap-6 bg-dots sm:flex-row sm:gap-0">
       <div class="flex h-12 basis-1/2 items-center border-black sm:border-b-4">
-        <h1 class="text-3xl font-black">Portfolio Website</h1>
+        <h1 class="text-2xl font-black md:text-3xl">Portfolio Website</h1>
       </div>
       <div class="grid h-12 basis-1/2 grid-cols-3 font-black">
         <NuxtLink
--- pages/stack.vue
+++ pages/stack.vue
@@ -4,13 +4,13 @@ import { techStack } from '../config'
 
 <template>
   <section class="flex flex-col items-center bg-yellow-200 p-12 lg:block">
-    <div class="card mb-12 w-72 bg-white">
+    <div class="card mb-12 bg-white lg:w-72">
       <div>
         <h3 class="mb-2 text-lg font-black">Techstack</h3>
         <p>Those are the technologies I work with on a daily basis. Some more, some less, depending on the project.</p>
       </div>
     </div>
-    <div class="grid grid-cols-1 justify-items-center gap-6 sm:grid-cols-2 lg:grid-cols-4">
+    <div class="grid grid-cols-1 justify-items-center gap-6 min-[530px]:grid-cols-2 lg:grid-cols-4">
       <StackCard v-for="tech in techStack" :key="tech.id" :tech="tech" />
     </div>
   </section>
